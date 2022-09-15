191c7f73c8913dfd0dee36291e2c9215b7d39964
pirmamatov <ogabekpirmamatov@gmail.com>
2022-09-16 00:30:49 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,243 @@
+
+// 1-masala
+
+function toSum(A){
+    if( A > 0 ){
+        console.log("Musbat son");
+    } else{
+        console.log("Manfiy son");
+    }
+}
+
+// 2-masala
+
+function toSum(A){
+    if(A % 2 == 1){
+        console.log("Toq son");
+    } else{
+        console.log("Juft son");
+    }
+}
+
+// 3 -masala 
+
+function toSum(A){
+    if( A % 2 == 0){
+        console.log("Juft son");
+    } else{
+        console.log("Toq son");
+    }
+}
+
+// 4-masala
+
+function toSum(A,B){
+    if(A > 0 && B > 0){
+        console.log("Rost");
+    } else{
+        console.log("Yolg'on");
+    }
+}
+
+// 5-masala
+
+function toSum(a,b,c){
+    if(a > b && b > c){
+        console.log("True");
+    } else{
+        console.log("False");
+    }
+}
+ // 6-masala
+
+function toSum(a,b,c){
+    if(a < b && b < c){
+        console.log("True");
+    } else{
+        console.log("False");
+    }
+}
+
+// 7-masala
+
+function toSum(a,b){
+   if( a % 2 == 1 && b % 2 == 1){
+    console.log("True");
+   } else{
+    console.log("False");
+   }
+}
+
+// 8-masala
+
+function toSum(a,b,c,d){
+    if(a > 0, b > 0, c > 0, d > 0){
+        console.log("Musbat son");
+    } else{
+        console.log("Manfiy son");
+    }
+}
+
+// 9-masala
+ 
+function toSum(a,b,c){
+    if(a > 0, b > 0, c < 0){
+        console.log("musbat");
+    } else if(a < 0, b > 0, c > 0){
+        console.log("musbat");
+    } else if(a > 0,b < 0, c > 0){
+        console.log("musbat");
+    } else{
+        console.log("manfiy");
+    }
+}
+
+// 10-mavzu
+
+// const day = []
+// day[0] = "Monday";
+// day[1] = "Tuesday";
+// day[2] = "Wednesday";
+// day[3] = "Thursday";
+// day[4] = "Friday";
+// day[5] = "Saturday";
+// day[6] = "Sunday"
+
+function week (day){
+if(day.includes("1")){
+    console.log("Monday");
+} else if (day.includes("2")){
+    console.log("Tuesday");
+}
+else if (day.includes("3")){
+    console.log("Wednesday");
+}
+else if (day.includes("4")){
+    console.log("Thursday");
+}
+else if (day.includes("5")){
+    console.log("Friday");
+}
+else if (day.includes("6")){
+    console.log("Saturday");
+} else if(day.includes("7")){
+    console.log("Sunday");
+}
+}
+
+// 11-masala
+
+function numberPhone (code){
+    if(code.includes("+99891 && +99890")){
+        console.log("Beeline");
+    } else if(code.includes("+99893")){
+        console.log("Ucell");
+    } else if(code.includes("+99897")){
+        console.log("UMC");
+    } else if (code.includes("+99899")){
+        console.log("UzMobile");
+    }
+}
+
+// 12-masala
+
+function toSum(a){
+    if(a > 0){
+        console.log("a + 1");
+    } else{
+        console.log("a - 1");
+    }
+}
+
+// 13-masala
+
+function toSum(a){
+    if(a > 0){
+        console.log("a + 3");
+    } else{
+        console.log("a - 2");
+    }
+}
+
+// 14-masala
+
+function toSum(a,b){
+    if(a > b){
+console.log("True");
+    } else{
+        console.log("False");
+    }
+}
+
+// 15-mavzu
+
+function toSum (a,b,c){
+    if(a > b > c){
+        console.log("a eng kattasi");
+    } else if (b > c > a){
+        console.log("b eng kattasi");
+    } else if(c > a > b) {
+        console.log("b eng kattasi");
+    }
+}
+
+// 16-masala
+
+function toSum (a,b,c){
+    if(a > b > c){
+        console.log("c eng kichigi");
+    } else if (b > c > a){
+        console.log("a eng kichigi");
+    } else if(c > a > b) {
+        console.log("b eng kichigi");
+    }
+}
+
+// 17-masala
+
+function picture (rasm){
+    if(rasm.includes("erkak")){
+        console.log("Mashinani rasmi");
+    } else { console.log("Qo'g'irchoqni rasmi");}
+}
+
+// 18-masala
+
+function stepindiya (baho){
+    if(baho.includes("5")){
+        console.log("555 ming so'm");
+    } else if(baho.includes("4")){
+        console.log("444 ming so'm");
+    } else if (baho.includes("3")){
+        console.log("333 ming so'm");
+    }
+}
+
+// 19-masala
+
+function toSum (a){
+    if(a > 0 && a % 2 == 1){
+        console.log("toq musbat");
+    } else if(a < 0 && a % 2 == 1){
+        console.log("toq manfiy");
+    } else if (a > 0 && a % 2 == 0){
+        console.log("Juft musbat");
+    } else if( a < 0 && a % 2 == 0){
+        console.log("juft manfiy");
+    }
+}
+
+
+// 20-masala
+
+
+function yosh (yil) {
+    if(yil.includes("2003")){
+        console.log("19 yosh");
+    } else if(yil.includes("2004")){
+        console.log("18 yosh");
+    }
+}
+
+// 21-masala
+
